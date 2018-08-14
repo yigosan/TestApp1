582ae8a812d2d27b34de0181dba65a3af8cd3e7f
Yigit Ersun <yigosan@hotmail.com>
2018-08-14 22:53:53 +0300

    Regex complete

--- TestApp1/Form1.Designer.cs
+++ TestApp1/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtPattern = new System.Windows.Forms.TextBox();
+            this.btnRegex = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 24);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(535, 104);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.Text = "/*!34 yigit */ ;\r\n/*!43 basak */ ; \r\n/*!22 ata */ ;\r\n/*!53 ates */ ;\r\n/*!34 yigit" +
+    " */ ;";
+            // 
+            // txtPattern
+            // 
+            this.txtPattern.Location = new System.Drawing.Point(12, 134);
+            this.txtPattern.Name = "txtPattern";
+            this.txtPattern.Size = new System.Drawing.Size(535, 20);
+            this.txtPattern.TabIndex = 1;
+            this.txtPattern.Text = "(\\/\\*![0-9]+ )(.*)(\\*\\/ ;)";
+            // 
+            // btnRegex
+            // 
+            this.btnRegex.Location = new System.Drawing.Point(553, 130);
+            this.btnRegex.Name = "btnRegex";
+            this.btnRegex.Size = new System.Drawing.Size(96, 24);
+            this.btnRegex.TabIndex = 2;
+            this.btnRegex.Text = "Regex";
+            this.btnRegex.UseVisualStyleBackColor = true;
+            this.btnRegex.Click += new System.EventHandler(this.btnRegex_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(12, 169);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(535, 104);
+            this.txtResults.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.btnRegex);
+            this.Controls.Add(this.txtPattern);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtPattern;
+        private System.Windows.Forms.Button btnRegex;
+        private System.Windows.Forms.TextBox txtResults;
     }
 }
 
--- TestApp1/Form1.cs
+++ TestApp1/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,23 @@ namespace TestApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnRegex_Click(object sender, EventArgs e)
+        {
+            string input = this.txtInput.Text;
+            string pattern = this.txtPattern.Text;
+            Regex reg = new Regex(pattern);           
+            //foreach(Group g in groups)
+            //    System.Diagnostics.Debug.WriteLine(g.Value.ToString().Trim());
+            foreach (Match match in reg.Matches(input))
+            {
+                GroupCollection groups = match.Groups;
+                input = input.Replace(groups[0].Value, groups[2].Value + ";");
+            }
+            this.txtResults.Text = input;
+            // System.Diagnostics.Debug.WriteLine("Replaced = " + input);
+            System.Diagnostics.Debug.WriteLine("done");
         }
     }
 }
